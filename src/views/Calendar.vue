a08c96af294a6ea917a0608adf53f1873d9ea2e4
Gaius08 <gaiusalfred@gmail.com>
2023-05-16 19:30:00 +0530

    changes done

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,36 +1,46 @@
 <template>
     <div class="pa-2">
         <v-autocomplete clearable class="pa-4 w-25" chips label="Select a country" :items="countries" item-text="name"
-            variant="solo-filled"></v-autocomplete>
+            v-model="countryName" variant="solo-filled"></v-autocomplete>
     </div>
     <div>
-        <VCalendar borderless :attributes='attrs' class="w-25 my-calendar" />
+        <VCalendar borderless :attributes="attrs" class="w-25 my-calendar" />
     </div>
+    <h1>{{ countryName }}</h1>
 </template>
+  
 <script>
 import axiosInstance from "../services/service.js";
 
 export default {
-    name: 'Calendar',
-
+    name: "Calendar",
     data() {
         return {
             countries: [],
+            countryName: "",
             attrs: [
                 {
-                    key: 'today',
+                    key: "today",
+                    // dot: true,
+                    // bar: true,
                     highlight: {
-                        backgroundColor: '#ff8080',
-                        // Other properties are available too, like `height` & `borderRadius`
+                        backgroundColor: "#ff8080",
+                        content: "red",
+                        highlight: "#E7F0FA5",
+
+                        // popover: { ... },
+                        // customData: { ... },
                     },
-                    dates: new Date()
-                }
+                    dates: new Date(),
+                },
             ],
         };
     },
+
     mounted() {
         this.selectCountry();
     },
+
     methods: {
         selectCountry() {
             axiosInstance
@@ -46,8 +56,11 @@ export default {
     },
 };
 </script>
+  
 <style>
-.my-calendar .vc-weekday-1, .my-calendar .vc-weekday-7 {
-  color: red;
+.my-calendar .vc-weekday-1,
+.my-calendar .vc-weekday-7 {
+    color: red;
 }
 </style>
+  
