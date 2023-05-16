f86668d559173e8c14b47c9ee1192dd57bfd3883
Gaius08 <gaiusalfred@gmail.com>
2023-05-16 18:10:30 +0530

    changes done

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,23 +1,31 @@
 <template>
-    
-        <div class="pa-2">
-            <v-autocomplete clearable class="pa-4 w-25" chips label="Select a country" :items="countries" item-text="name"
-                variant="solo-filled"></v-autocomplete>
-        </div>
-        <div>
-            <VCalendar borderless  class="w-25" />
-        </div>
-   
+    <div class="pa-2">
+        <v-autocomplete clearable class="pa-4 w-25" chips label="Select a country" :items="countries" item-text="name"
+            variant="solo-filled"></v-autocomplete>
+    </div>
+    <div>
+        <VCalendar borderless :attributes='attrs' class="w-25 my-calendar" />
+    </div>
 </template>
 <script>
 import axiosInstance from "../services/service.js";
 
 export default {
     name: 'Calendar',
-    
+
     data() {
         return {
             countries: [],
+            attrs: [
+                {
+                    key: 'today',
+                    highlight: {
+                        backgroundColor: '#ff8080',
+                        // Other properties are available too, like `height` & `borderRadius`
+                    },
+                    dates: new Date()
+                }
+            ],
         };
     },
     mounted() {
@@ -38,3 +46,8 @@ export default {
     },
 };
 </script>
+<style>
+.my-calendar .vc-weekday-1, .my-calendar .vc-weekday-7 {
+  color: red;
+}
+</style>
