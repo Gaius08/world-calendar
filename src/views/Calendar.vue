b02731b70d5d5d514916242787605bb3f747f431
Gaius08 <gaiusalfred@gmail.com>
2023-05-17 16:16:48 +0530

    changes done

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,65 +1,109 @@
 <template>
     <div class="pa-2">
-        <v-autocomplete clearable class="pa-4 w-25" chips label="Select a country" :items="countries" v-model="countryName"
-            item-title="name" item-value="code" variant="solo"></v-autocomplete>
+      <v-autocomplete
+        clearable
+        class="pa-4 w-25"
+        chips
+        label="Select a country"
+        :items="countries"
+        v-model="countryName"
+        item-title="name"
+        item-value="code"
+        variant="solo"
+      ></v-autocomplete>
     </div>
     <div>
-        <VCalendar borderless :attributes="attrs" class="w-25 my-calendar" :active-date="activeDate" />
+      <VCalendar
+        borderless
+        :attributes="computedAttributes"
+        class="w-25 my-calendar"
+        :active-date="activeDate"
+      />
     </div>
     <h1>{{ countryName }}</h1>
-    <button @click="printCurrentYear">Print Current Year</button>
-</template>
+    <button @click="fetchPublicHolidays">Fetch Public Holidays</button>
+  </template>
   
-<script>
-import axiosInstance from "../services/service.js";
-
-export default {
+  <script>
+  import axiosInstance from "../services/service.js";
+  
+  export default {
     name: "Calendar",
     data() {
-        return {
-            countries: [],
-            countryName: "",
-            attrs: [
-                {
-                    key: "today",
-                    highlight: {
-                        backgroundColor: "#ff8080",
-                        content: "red",
-                        highlight: "#E7F0FA5",
-                    },
-                    dates: new Date(),
-                },
-            ],
-        };
+      return {
+        countries: [],
+        countryName: "",
+        activeDate: new Date(),
+        publicHolidays: [],
+        attrs: [
+          {
+            key: "today",
+            highlight: {
+              backgroundColor: "#ff8080",
+              content: "red",
+              highlight: "#E7F0FA5",
+            },
+            dates: new Date(),
+            popover: {
+            label:""
+          },
+          },
+          
+        ],
+      };
     },
     mounted() {
-        this.selectCountry();
+      this.selectCountry();
+    },
+    computed: {
+      computedAttributes() {
+        return [...this.attrs, ...this.publicHolidays];
+      },
     },
     methods: {
-        selectCountry() {
-            axiosInstance
-                .get("AvailableCountries")
-                .then((response) => {
-                    console.log("Response from server:", response.data);
-                    response.data.map((country) => (
-                        this.countries.push({ name: country.name, code: country.countryCode })
-                    ));
-                    console.log("this.countries", this.countries)
-                })
-                .catch((error) => {
-                    console.log("Error from server:", error);
-                });
-        },
-        printCurrentYear() {
-            const currentYear = new Date(this.activeDate).getFullYear();
-            console.log(currentYear);
-        },
+      selectCountry() {
+        axiosInstance
+          .get("AvailableCountries")
+          .then((response) => {
+            this.countries = response.data.map((country) => ({
+              name: country.name,
+              code: country.countryCode,
+            }));
+          })
+          .catch((error) => {
+            console.log("Error from server:", error);
+          });
+      },
+      fetchPublicHolidays() {
+        axiosInstance
+          .get("NextPublicHolidays/" + this.countryName)
+          .then((response) => {
+            console.log("Response from server:", response.data);
+            this.publicHolidays = response.data.map((holiday) => {
+              return {
+                key: holiday.date,
+                highlight: {
+                  content: holiday.name,
+                  backgroundColor: "#ff8080",
+                  highlight: "red",
+                  
+                },
+                dates: new Date(holiday.date),
+              };
+            });
+          })
+          .catch((error) => {
+            console.log("Error from server:", error);
+          });
+      },
     },
-};
-</script>
-<style>
-.my-calendar .vc-weekday-1,
-.my-calendar .vc-weekday-7 {
+  };
+  </script>
+  
+  <style>
+  .my-calendar .vc-weekday-1,
+  .my-calendar .vc-weekday-7 {
     color: red;
-}
-</style>
+  }
+  </style>
+  
